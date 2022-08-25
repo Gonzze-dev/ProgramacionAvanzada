0997b7627c239c49ae9b0ea1e232ce1b55269ebb
Gonzalo-PC <errandoneagonzalo18@gmail.com>
2022-08-24 23:07:42 -0300

    tarea hecha

---
+++ Tarea/clima.js
@@ -0,0 +1,30 @@
+const ciudad = "Concepcion del Uruguay"
+const pais = "ARG"
+
+const api_key = "bca4ea417c456fd17c8bb40d0f1e4bd2"
+const api = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${api_key}`
+
+ async function getData () {
+
+  try {
+    const resp = await fetch(api)
+    const data = await resp.json()
+
+    const humedad = data.main.humidity
+    const temperatura = Math.round(data.main.temp - 273.15)
+    const presion = data.main.pressure
+
+    let info = document.createElement('div')
+
+    document.body.innerHTML = "<h1> Weahter in " + data.name + "</h1>"
+    info = `temperatura ${temperatura} C° - humedad ${humedad} - preison ${presion}`
+    
+    document.body.append(info)
+  } catch (error) {
+    console.error('Hubo un error en la peticion', error)
+  }
+
+}
+
+getData()
+console.log("hola")
